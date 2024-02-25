ce2b8e09e95699a093b4eeb5a210aecb40990353
Yoshiip <myoshipro@gmail.com>
2024-02-25 14:05:21 +0100

    Enhanced projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,157 +1,205 @@
 <script>
-// @ts-nocheck
+  // @ts-nocheck
 
+  import projects from "$lib/projects.json";
+  import ProjectModal from "./ProjectModal.svelte";
 
-import projects from '$lib/projects.json';
-import ProjectModal from './ProjectModal.svelte'
+  let projectId = 0;
 
-var projectId = 0;
+  let modal_visible = false;
 
-let modal_visible = false;
+  let disabledScroll = false;
 
-let disabledScroll = false;
+  let listYears = [];
 
-
-/**
- * @param {number} id
- */
-function showProject(id) {
+  /**
+   * @param {number} id
+   */
+  function showProject(id) {
     modal_visible = true;
-    projectId = id
+    projectId = id;
     disabledScroll = true;
-}
+  }
 
-function closeModal() {
+  function closeModal() {
     modal_visible = false;
     disabledScroll = false;
-}
-
+  }
 </script>
-
 
 <title>Projets • Tourneur Aymeri</title>
 <main class:scroll-lock={disabledScroll}>
-    
-    <img src="/images/projects/projects_header.jpg" alt="Projects" class="header">
-    <h1>Tous mes projets ({projects.length})</h1> 
-    <div class="projects_container" id="projects">
-        {#each projects as project, i}
-            <button class="project" on:click={() => showProject(i)}>
-              <img src="{project.images[0]}" alt="" class="cover">
-                <div class="name">{project.name}</div>
-                <div class="date">{project.date.split("-")[0]}</div>
-            </button>
-        {/each}
-    </div>
-
-
+  <img
+    src="/images/projects/projects_header.jpg"
+    alt="Projects"
+    class="header"
+  />
+  <h1>Tous mes projets ({projects.length})</h1>
+  <div class="projects_container" id="projects">
+    {#each projects as project, i}
+      {#if !listYears.includes(project.date.split("-")[0]) && listYears.push(project.date.split("-")[0])}
+        <div class="year">
+          <h2>{project.date.split("-")[0]}</h2>
+          <p>
+            {projects.filter(
+              (p) => p.date.split("-")[0] === project.date.split("-")[0]
+            ).length} projets
+          </p>
+        </div>
+      {/if}
+      <button class="project" on:click={() => showProject(i)}>
+        <img src={project.images[0]} alt="" class="cover" />
+        <div class="name">{project.name}</div>
+        <div class="date">{project.type}</div>
+      </button>
+    {/each}
+  </div>
 </main>
 
 {#if modal_visible}
-    <ProjectModal closeProjectModal={closeModal} projectId={projectId}/>
+  <ProjectModal closeProjectModal={closeModal} {projectId} />
 {/if}
 
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
 
-    main {
-      margin-top: 80px;
-      text-align: center;
-    }
-
-    h1 {
-      margin: 0;
-    }
-
-    .header {
-      width: calc(100% - 64px);
-      max-width: 800px;
-      border-radius: 16px;
-      margin-bottom: 64px;
-      box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
-    }
-
-
-.scroll-lock {
-  overflow-y: hidden;
-}
-
-.projects_container {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr)); 
-  gap: 8px;
-  padding-top: 64px;
-  padding-left: 32px;
-  padding-right: 32px;
-  padding-bottom: 100px;
-}
-
-.project {
-  width: 320px;
-  position: relative;
-  border: none;
-
-  border-radius: 8px;
-  cursor: pointer;
-
-  align-self: center;
-  justify-self: center;
-  transition: 0.15s ease;
-
-  background-color: rgb(27, 28, 29);
-
-  font-size: 16px;
-}
-
-.project > .cover {
-  width: calc(100% - 16px);
-  object-fit: contain;
-  aspect-ratio: 16 / 9;
-  margin-top: 8px;
-}
-
-.project > .name {
-  color: white;
-  text-align: left;
-  margin: 8px;
-  font-weight: 700;
-  font-family: 'Space Mono', monospace;
-}
-
-.project > .date {
-  position: absolute;
-  right: 14px;
-  bottom: 8px;
-  background-color: blue;
-  padding: 2px;
-  border-radius: 4px;
-  color: rgb(255, 255, 255);
-  font-weight: 700;
-  font-family: 'Space Mono', monospace;
-}
-
-@media screen and (max-width: 1024px) {
-  .projects_container {
-    grid-template-columns: repeat(2, 1fr);
+  main {
+    margin-top: 80px;
+    text-align: center;
   }
 
-  .project {
-    width: calc(50svw - 32px);
+  h1 {
+    margin: 0;
   }
-}
 
-@media screen and (max-width: 640px) {
+  .header {
+    width: calc(100% - 64px);
+    max-width: 800px;
+    border-radius: 16px;
+    margin-bottom: 64px;
+    box-shadow:
+      rgba(50, 50, 93, 0.25) 0px 30px 60px -12px,
+      rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
+  }
+
+  .scroll-lock {
+    overflow-y: hidden;
+  }
+
   .projects_container {
-    grid-template-columns: 1fr;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    gap: 8px;
+    padding-top: 64px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 100px;
+  }
+
+  .projects_container > div {
+    background-image: linear-gradient(
+      to right bottom,
+      #2c00ff,
+      #231afb,
+      #1a27f6,
+      #1431f1,
+      #1238eb
+    );
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 16px;
+    justify-content: end;
+    font-family: "Space Mono", monospace;
+    border-radius: 8px;
+    color: white;
+  }
+
+  .projects_container > div > h2 {
+    margin: 0;
     padding: 0;
+    font-size: 3em;
+  }
+
+  .projects_container > div > p {
+    margin: 0;
+    padding: 0;
+    font-weight: 900;
   }
 
   .project {
-    width: calc(100svw - 64px);
-  }
-}
+    position: relative;
+    border: none;
 
-.project:hover {
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
-}
+    border-radius: 8px;
+    cursor: pointer;
+
+    align-self: center;
+    justify-self: center;
+    transition: 0.15s ease;
+
+    background-color: rgb(27, 28, 29);
+    font-size: 16px;
+  }
+
+  .project > .cover {
+    width: calc(100% - 16px);
+    object-fit: contain;
+    aspect-ratio: 16 / 9;
+    margin-top: 8px;
+  }
+
+  .project > .name {
+    color: white;
+    text-align: left;
+    margin: 8px;
+    font-weight: 700;
+    font-family: "Space Mono", monospace;
+  }
+
+  .project > .date {
+    position: absolute;
+    right: 14px;
+    bottom: 8px;
+    background-image: linear-gradient(
+      to right bottom,
+      #2c00ff,
+      #231afb,
+      #1a27f6,
+      #1431f1,
+      #1238eb
+    );
+    padding: 2px;
+    border-radius: 4px;
+    color: rgb(255, 255, 255);
+    font-weight: 700;
+    font-family: "Space Mono", monospace;
+  }
+
+  @media screen and (max-width: 1024px) {
+    .projects_container {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    .project {
+      width: calc(50svw - 32px);
+    }
+  }
+
+  @media screen and (max-width: 640px) {
+    .projects_container {
+      grid-template-columns: 1fr;
+      padding: 0;
+    }
+
+    .project {
+      width: calc(100svw - 64px);
+    }
+  }
+
+  .project:hover {
+    box-shadow:
+      rgba(50, 50, 93, 0.25) 0px 30px 60px -12px,
+      rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
+  }
 </style>
--- src/routes/projects/ProjectModal.svelte
+++ src/routes/projects/ProjectModal.svelte
@@ -1,170 +1,200 @@
 <script>
-// @ts-nocheck
+  // @ts-nocheck
 
-import { fade, blur, fly, slide, scale, draw, crossfade } from 'svelte/transition';
-import projects from '$lib/projects.json';
-import tools from '$lib/tools.json';
+  import {
+    fade,
+    blur,
+    fly,
+    slide,
+    scale,
+    draw,
+    crossfade,
+  } from "svelte/transition";
+  import projects from "$lib/projects.json";
+  import tools from "$lib/tools.json";
 
+  export let projectId = 0;
 
+  /**
+   * @type {null}
+   */
+  export let closeProjectModal = null;
 
-export let projectId = 0;
-
-/**
-     * @type {null}
-     */
- export let closeProjectModal = null;
-
-/**
-     * @param {string} property_label
-     * @param {string} value
-     */
-function add_formatted(property_label, value) {
-    var data = [property_label, value]
-    if(value.toLowerCase() in ICONS) {
-        data.push(ICONS[value.toLowerCase()]);
+  /**
+   * @param {string} property_label
+   * @param {string} value
+   */
+  function add_formatted(property_label, value) {
+    let data = [property_label, value];
+    if (value.toLowerCase() in ICONS) {
+      data.push(ICONS[value.toLowerCase()]);
     }
     return data;
-}
+  }
 
-const PROJECTS_TYPES = {"game": "Jeu"}
+  const PROJECTS_TYPES = { game: "Jeu" };
 
-let carousel; // for calling methods of the carousel instance
+  let carousel; // for calling methods of the carousel instance
 
-
-/**
-     * @param {string | number | Date} date
-     */
-function format_date(date) {
-    var d = new Date(date);
-    return d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
-}
-
-
-    /**
-     * @param {{ code: any; }} e
-     */
-function keyDown(e) {
+  /**
+   * @param {{ code: any; }} e
+   */
+  function keyDown(e) {
     console.log(e);
-    switch(e.code) {
-        case "Escape":
-            closeProjectModal();
-        }
-}
+    switch (e.code) {
+      case "Escape":
+        closeProjectModal();
+    }
+  }
 
-
-
-    /**
-     * @param {number} delta
-     */
-function offsetProjectId(delta) {
-    projectId = ((projectId + delta) % projects.length + projects.length) % projects.length
-}
-
+  /**
+   * @param {number} delta
+   */
+  function offsetProjectId(delta) {
+    projectId =
+      (((projectId + delta) % projects.length) + projects.length) %
+      projects.length;
+  }
 </script>
 
-<svelte:window on:keydown={keyDown}/>
+<svelte:window on:keydown={keyDown} />
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="black_background" on:click|self={closeProjectModal} transition:fade>
-    <div class="modal" transition:scale>
-        <div class="content">
-            <div class="project_info_list desktop">
-                <h2>{projects[projectId].name}</h2>
-                <div class="year">{projects[projectId].type} • {projects[projectId].date.split('-')[0]}</div>
-                <ul>
-                    <li>
-                        <a href="#description">Description</a>
-                    </li>
-                    <li>
-                        <a href="#galerie">Galerie</a>
-                    </li>
-                    <li>
-                        <a href="#technologies">Technologies</a>
-                    </li>
-                    <li>
-                        <a href="#liens">Liens</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="modal_container">
-                <div class="modal_header">
-                    <div class="name">
-                        {#if "logo" in projects[projectId]}
-                            <img src={projects[projectId].logo} alt="Project Logo" class="project_logo">
-                        {:else}
-                            <h2>{projects[projectId].name}</h2>
-                        {/if}
-                    </div>
-                </div>
-                <h2 id="description">Description</h2>
-                <p>{@html projects[projectId].description}</p>
-                <h2 id="galerie">Galerie</h2>
-                <div class="images">
-                    {#each projects[projectId].images as image_src}
-                        <img src={image_src} on:click={() => window.open(image_src, '_blank')} alt="Media">
-                    {/each}
-                </div>
-                <h2 id="technologies">Technologies</h2>
-                <div class="techno">
-                    {#each Object.keys(projects[projectId].tools) as tool}
-                    <div>
-                        {#if Object.hasOwn(tools, projects[projectId].tools[tool])}
-                            {#if Object.hasOwn(tools[projects[projectId].tools[tool]], "icon")}
-                                <img src={"/images/tools/" + tools[projects[projectId].tools[tool]].icon} alt="" srcset="">
-                            {:else}
-                                <img src={"/images/tools/" + projects[projectId].tools[tool] + ".svg"} alt="" srcset="">
-                            {/if}    
-                                <div class="label">{tool}</div>
-                            {#if Object.hasOwn(tools[projects[projectId].tools[tool]], "name")}    
-                                <span>{tools[projects[projectId].tools[tool]].name}</span>
-                            {:else}
-                                <span>{projects[projectId].tools[tool]}</span>
-                            {/if}
-                        {:else}
-                            <span>Erreur!</span>
-                        {/if}
-                    </div>
-                    {/each}
-                </div>
-                <h2 id="liens">Liens</h2>
-                <div class="buttons_container">
-                    <button class="button play_btn" on:click={() => open(projects[projectId].link)}>
-                        {projects[projectId].button_label}
-                        <img src="/images/icons/open.svg" alt="" srcset="">
-                    </button>
-                    {#if projects[projectId].github_link}
-                        <button class="button github_btn" on:click={() => open(projects[projectId].github_link)} alt="Github"><img alt="Github" src="/images/icons/github_black.png" width="24px"></button>
-                    {/if}
-                </div>
-            </div>
+  <div class="modal" transition:scale>
+    <div class="content">
+      <div class="project_info_list desktop">
+        <h2>{projects[projectId].name}</h2>
+        <div class="year">
+          {projects[projectId].type} • {projects[projectId].date.split("-")[0]}
         </div>
-        <button class="close_btn" on:click={closeProjectModal}>
-            <img src="/images/icons/close.svg" alt="Close" srcset="">
-        </button>
+        <ul>
+          <li>
+            <a href="#description">Description</a>
+          </li>
+          <li>
+            <a href="#galerie">Galerie</a>
+          </li>
+          <li>
+            <a href="#technologies">Technologies</a>
+          </li>
+          <li>
+            <a href="#liens">Liens</a>
+          </li>
+        </ul>
+      </div>
+      <div class="modal_container">
+        <div class="modal_header">
+          <div class="name">
+            {#if "logo" in projects[projectId]}
+              <img
+                src={projects[projectId].logo}
+                alt="Project Logo"
+                class="project_logo"
+              />
+            {:else}
+              <h2>{projects[projectId].name}</h2>
+            {/if}
+          </div>
+        </div>
+        <h2 id="description">Description</h2>
+        <p>{@html projects[projectId].description}</p>
+        <h2 id="galerie">Galerie</h2>
+        <div class="images">
+          {#each projects[projectId].images as image_src}
+            <img
+              src={image_src}
+              on:click={() => window.open(image_src, "_blank")}
+              alt="Media"
+            />
+          {/each}
+        </div>
+        <h2 id="technologies">Technologies</h2>
+        <div class="techno">
+          {#each Object.keys(projects[projectId].tools) as tool}
+            <div>
+              {#if Object.hasOwn(tools, projects[projectId].tools[tool])}
+                {#if Object.hasOwn(tools[projects[projectId].tools[tool]], "icon")}
+                  <img
+                    src={"/images/tools/" +
+                      tools[projects[projectId].tools[tool]].icon}
+                    alt=""
+                    srcset=""
+                  />
+                {:else}
+                  <img
+                    src={"/images/tools/" +
+                      projects[projectId].tools[tool] +
+                      ".svg"}
+                    alt=""
+                    srcset=""
+                  />
+                {/if}
+                <div class="label">{tool}</div>
+                {#if Object.hasOwn(tools[projects[projectId].tools[tool]], "name")}
+                  <span>{tools[projects[projectId].tools[tool]].name}</span>
+                {:else}
+                  <span>{projects[projectId].tools[tool]}</span>
+                {/if}
+              {:else}
+                <span>Erreur!</span>
+              {/if}
+            </div>
+          {/each}
+        </div>
+        <h2 id="liens">Liens</h2>
+        <div class="buttons_container">
+          <button
+            class="button play_btn"
+            on:click={() => open(projects[projectId].link)}
+          >
+            {projects[projectId].button_label}
+            <img src="/images/icons/open.svg" alt="" srcset="" />
+          </button>
+          {#if projects[projectId].github_link}
+            <button
+              class="button github_btn"
+              on:click={() => open(projects[projectId].github_link)}
+              alt="Github"
+              ><img
+                alt="Github"
+                src="/images/icons/github_black.png"
+                width="24px"
+              /></button
+            >
+          {/if}
+        </div>
+      </div>
     </div>
-    <div class="left_position arrow desktop" on:click={() => offsetProjectId(-1)}>
-        <img src="/images/icons/right-arrow.png" alt="arrow icon">
-    </div>
-    <div class="right_position arrow desktop" on:click={() => offsetProjectId(1)}>
-        <img alt="Next project" src="/images/icons/right-arrow.png">
-    </div>
+    <button class="close_btn" on:click={closeProjectModal}>
+      <img src="/images/icons/close.svg" alt="Close" srcset="" />
+    </button>
+  </div>
+  <div class="left_position arrow desktop" on:click={() => offsetProjectId(-1)}>
+    <img src="/images/icons/right-arrow.png" alt="arrow icon" />
+  </div>
+  <div class="right_position arrow desktop" on:click={() => offsetProjectId(1)}>
+    <img alt="Next project" src="/images/icons/right-arrow.png" />
+  </div>
 </div>
 
 <style>
-    .modal {
-        width: 80%;
-        height: 80%;
-        background-color: #D9D9D9;
-        border-radius: 12px;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-        position: relative;
-        
-        display: flex;
-        flex-direction: column;
-        font-family: 'Inter', sans-serif;
-        z-index: 1;
-    }
+  .modal {
+    width: 80%;
+    height: 80%;
+    background-color: #d9d9d9;
+    border-radius: 12px;
+    box-shadow:
+      rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+      rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+    position: relative;
 
-.black_background {
+    display: flex;
+    flex-direction: column;
+    font-family: "Inter", sans-serif;
+    z-index: 1;
+  }
+
+  .black_background {
     position: fixed;
     left: 0;
     top: 0;
@@ -175,64 +205,65 @@ function offsetProjectId(delta) {
     align-items: center;
     justify-content: center;
     z-index: 1;
-}
+  }
 
-.project_info_list {
+  .project_info_list {
     background-color: #0f0f10;
     color: white;
     min-width: 320px;
     margin: 12px;
     border-radius: 12px;
     height: calc(100% - 24px);
-}
+  }
 
-.project_info_list li, ul, a {
+  .project_info_list li,
+  ul,
+  a {
     text-decoration: none;
     color: white;
     margin: 4px;
-}
+  }
 
-.project_info_list h2 {
+  .project_info_list h2 {
     text-align: center;
     font-size: 24px;
     margin-top: 64px;
     margin-bottom: 0;
-}
+  }
 
-.project_info_list .year {
+  .project_info_list .year {
     text-align: center;
     color: lightgray;
-}
+  }
 
-.project_info_list > ul {
+  .project_info_list > ul {
     margin: 8px;
     padding: 4px;
-}
+  }
 
-.project_info_list > ul > li {
+  .project_info_list > ul > li {
     padding: 4px;
     font-weight: 100;
     margin: 0;
-}
+  }
 
-.project_info_list > ul > li:hover {
+  .project_info_list > ul > li:hover {
     padding: 4px;
     background-color: rgb(26, 26, 26);
     border-radius: 4px;
     list-style-type: none;
     font-weight: 100;
-}
+  }
 
-
-.modal_header {
+  .modal_header {
     position: relative;
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-}
+  }
 
- .name {
+  .name {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -243,58 +274,53 @@ function offsetProjectId(delta) {
     margin-top: 64px;
     border: 2px solid rgba(0, 0, 255, 0.404);
     box-shadow: 0px 2px 16px 0px rgba(0, 0, 255, 0.486);
-}
+  }
 
-.name > h2 {
+  .name > h2 {
     font-size: 32px;
-    color: #00F;
+    color: #00f;
     font-weight: 900;
     margin: 0;
     padding: 0;
-}
+  }
 
-.content {
+  .content {
     margin: 0;
     padding: 0;
     display: inline-flex;
     height: 100%;
     width: 100%;
-}
+  }
 
-
-
-
-@media only screen and (max-width: 1024px) {
+  @media only screen and (max-width: 1024px) {
     .modal {
-        width: 100%;
-        height: 100%;
-        border-radius: 0px;
+      width: 100%;
+      height: 100%;
+      border-radius: 0px;
     }
 
     .content {
-        width: 100%;
+      width: 100%;
     }
 
-
-
     .project_info_list {
-        padding-bottom: 200px;
-        position: initial;
-        height: min-content;
-        order: 3;
+      padding-bottom: 200px;
+      position: initial;
+      height: min-content;
+      order: 3;
     }
 
     .close_btn {
-        position: absolute;
-        top: 32px;
-        right: 32px;
+      position: absolute;
+      top: 32px;
+      right: 32px;
     }
     .modal_header {
-        padding-top: 96px;
+      padding-top: 96px;
     }
-}
+  }
 
-.close_btn {
+  .close_btn {
     position: absolute;
     right: 32px;
     top: 64px;
@@ -312,46 +338,42 @@ function offsetProjectId(delta) {
     display: flex;
     align-items: center;
     justify-content: center;
-}
+  }
 
-
-.close_btn > img {
+  .close_btn > img {
     filter: invert();
     width: 48px;
     height: 48px;
-}
+  }
 
-.images {
+  .images {
     width: 100%;
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
     gap: 8px;
+  }
 
-    
-}
-
-.images > img {
+  .images > img {
     border-radius: 10px;
     width: 320px;
-}
+  }
 
-
-.buttons_container {
+  .buttons_container {
     display: flex;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: 96px;
-}
+  }
 
-.button {
+  .button {
     background-color: gray;
     color: white;
     width: 200px;
     height: 40px;
-}
+  }
 
-.play_btn {
+  .play_btn {
     background-color: blue;
     border: none;
     position: relative;
@@ -359,72 +381,67 @@ function offsetProjectId(delta) {
     color: white;
     width: 240px;
     margin-right: 20px;
-    font-family: 'Space Mono', monospace;
-}
+    font-family: "Space Mono", monospace;
+  }
 
-.play_btn > img {
+  .play_btn > img {
     position: absolute;
     right: 4px;
     top: 4px;
-}
+  }
 
-.play_btn:hover {
+  .play_btn:hover {
     background-color: rgb(52, 52, 235);
-}
+  }
 
-.github_btn {
+  .github_btn {
     width: 40px;
     border: none;
     border-radius: 8px;
     background-color: white;
-}
+  }
 
-
-
-.modal_container {
+  .modal_container {
     width: 100%;
     max-height: 100%;
     overflow-y: auto;
-}
+  }
 
-
-.modal_container > p {
+  .modal_container > p {
     padding: 8px;
-}
+  }
 
-
-.project_logo {
+  .project_logo {
     height: calc(100% - 16px);
-}
+  }
 
-
-.arrow {
+  .arrow {
     position: absolute;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
-}
+  }
 
-.left_position {
+  .left_position {
     left: 20px;
-}
+  }
 
-.left_position > img {
+  .left_position > img {
     transform: scaleX(-1);
-}
+  }
 
-.right_position {
+  .right_position {
     right: 20px;
-}
+  }
 
-.techno {
+  .techno {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     gap: 8px;
-}
+  }
 
-.techno > div {
+  .techno > div {
     width: 200px;
     min-height: 96px;
     background-color: white;
@@ -432,28 +449,26 @@ function offsetProjectId(delta) {
     padding: 8px;
     position: relative;
     animation: 0.1s;
-}
+  }
 
-.techno > div:hover {
+  .techno > div:hover {
     width: 200px;
     background-color: white;
     border-radius: 8px;
     position: relative;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-}
+  }
 
-.techno > div > img {
+  .techno > div > img {
     width: 40px;
     max-height: 40px;
-}
+  }
 
-
-.techno > div > .label {
+  .techno > div > .label {
     color: gray;
-}
-.techno > div > span {
+  }
+  .techno > div > span {
     font-size: 1.25em;
     font-weight: 900;
-}
-
+  }
 </style>

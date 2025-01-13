38e38b32af6a043f5cf4aa1d1641a73dc3b55a11
Aymeri Tourneur <myoshipro@gmail.com>
2025-01-13 16:29:48 +0100

    backup before svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
-    import { onNavigate } from "$app/navigation";
-    import "tailwindcss/tailwind.css";
+  import { onNavigate } from "$app/navigation";
+  import "tailwindcss/tailwind.css";
 
-    onNavigate((navigation) => {
-        // @ts-ignore
-        if (!document.startViewTransition) return;
+  onNavigate((navigation) => {
+    // @ts-ignore
+    if (!document.startViewTransition) return;
 
-        return new Promise((resolve) => {
-            // @ts-ignore
-            document.startViewTransition(async () => {
-                resolve();
-                await navigation.complete;
-            });
-        });
+    return new Promise((resolve) => {
+      // @ts-ignore
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
     });
+  });
 </script>
+
+<svelte:head>
+  <script
+    defer
+    data-domain="aymeri.dev"
+    src="https://plausible.aymeri.dev/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js"
+  ></script>
+  <script>
+    window.plausible =
+      window.plausible ||
+      function () {
+        (window.plausible.q = window.plausible.q || []).push(arguments);
+      };
+  </script>
+</svelte:head>
 
 <slot></slot>
